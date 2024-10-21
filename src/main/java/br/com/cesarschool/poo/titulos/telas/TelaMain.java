775d034f0c3185e14cb00fe900ada36d934e3c6c
Gabriel Q S Landim <142419701+Gabrielqlandim@users.noreply.github.com>
2024-10-20 21:52:52 -0300

    crud feito

--- src/main/java/br/com/cesarschool/poo/titulos/telas/TelaMain.java
+++ src/main/java/br/com/cesarschool/poo/titulos/telas/TelaMain.java
@@ -4,52 +4,50 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 
 public class TelaMain extends JPanel{
+
     JFrame frame = new JFrame();
+
+
+
 
     JButton alterar = new JButton("alterar");
     JButton buscar = new JButton("buscar");
     JButton excluir = new JButton("excluir");
     JButton incluir = new JButton("incluir");
 
+
+
     private void initialize(){
-        setLayout(null);
+        frame.setTitle("Inicio");
+        frame.setSize(600, 500);
+        frame.setLayout(null);
+
         //buscar
         buscar.setBounds(75, 200, 100, 25);
         frame.add(buscar);
         buscar.addActionListener(e -> buscar(e));
 
         //excluir
-        excluir.setBounds(75, 200, 100, 25);
+        excluir.setBounds(200, 200, 100, 25);
         frame.add(excluir);
         excluir.addActionListener(e -> excluir(e));
 
         //incluir
-        incluir.setBounds(75, 200, 100, 25);
+        incluir.setBounds(325, 200, 100, 25);
         frame.add(incluir);
         incluir.addActionListener(e -> incluir(e));
 
         //alterar
-        alterar.setBounds(75, 200, 100, 25);
+        alterar.setBounds(450, 200, 100, 25);
         frame.add(alterar);
         alterar.addActionListener(e -> alterar(e));
+        frame.setVisible(true);
+
     }
 
     public TelaMain(){
         initialize();
-
-        frame.setTitle("Tela Principal");
-        frame.setSize(600, 500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-
-        frame.add(this.buscar);
-        frame.add(this.excluir);
-        frame.add(this.alterar);
-        frame.add(this.incluir);
-
-
-
-        frame.setVisible(true);
     }
 
 
