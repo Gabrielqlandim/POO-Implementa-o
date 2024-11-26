9328fa216e56eeb8c3debecfe5ed5732e36d84ed
Gabriel Q S Landim <142419701+Gabrielqlandim@users.noreply.github.com>
2024-11-25 23:00:49 -0300

    tudo finalizado

--- src/main/java/br/com/cesarschool/poo/titulos/telas/Main.java
+++
@@ -1,7 +0,0 @@
-package br.com.cesarschool.poo.titulos.telas;
-
-public class Main {
-    public static void main(String[] args) {
-        TelaMain telaMain = new TelaMain();
-    }
-}
--- src/main/java/br/com/cesarschool/poo/titulos/utils/Comparavel.java
+++
@@ -1,5 +0,0 @@
-package br.com.cesarschool.poo.titulos.utils;
-
-public interface Comparavel {
-    public int comparar(Comparavel c);
-}
